49cc1a1f967419aa47bcdbe77c34fd5b9ad0fd8e
DivaliDvl <63827056+DivaliDvl@users.noreply.github.com>
2020-04-23 11:06:53 +0200

    Update voice.cc

--- yarns/voice.cc
+++ yarns/voice.cc
@@ -251,4 +251,160 @@ uint16_t Voice::trigger_dac_code() const {
   }
 }
 
+  static const uint16_t kHighestNote = 128 * 128;
+static const uint16_t kPitchTableStart = 116 * 128;
+
+
+void Oscillator::Init(int32_t scale, int32_t offset) {
+  audio_buffer_.Init();
+  phase_ = 0;
+  next_sample_ = 0;
+  high_ = false;
+  scale_ = scale;
+  offset_ = offset;
+  integrator_state_ = 0;
+}
+
+uint32_t Oscillator::ComputePhaseIncrement(int16_t midi_pitch) {
+  if (midi_pitch >= kHighestNote) {
+    midi_pitch = kHighestNote - 1;
+  }
+
+  int32_t ref_pitch = midi_pitch;
+  ref_pitch -= kPitchTableStart;
+
+  size_t num_shifts = 0;
+  while (ref_pitch < 0) {
+    ref_pitch += kOctave;
+    ++num_shifts;
+  }
+
+  uint32_t a = lut_oscillator_increments[ref_pitch >> 4];
+  uint32_t b = lut_oscillator_increments[(ref_pitch >> 4) + 1];
+  uint32_t phase_increment = a + \
+      (static_cast<int32_t>(b - a) * (ref_pitch & 0xf) >> 4);
+  phase_increment >>= num_shifts;
+  return phase_increment;
+}
+
+void Oscillator::RenderSilence() {
+  size_t size = kAudioBlockSize;
+  while (size--) {
+    audio_buffer_.Overwrite(offset_);
+  }
+}
+
+void Oscillator::RenderSine(uint32_t phase_increment) {
+  size_t size = kAudioBlockSize;
+  while (size--) {
+    phase_ += phase_increment;
+    int32_t sample = Interpolate1022(wav_sine, phase_);
+    audio_buffer_.Overwrite(offset_ - (scale_ * sample >> 16));
+  }
+}
+
+void Oscillator::RenderNoise() {
+  size_t size = kAudioBlockSize;
+  while (size--) {
+    int16_t sample = Random::GetSample();
+    audio_buffer_.Overwrite(offset_ - (scale_ * sample >> 16));
+  }
+}
+
+void Oscillator::RenderSaw(uint32_t phase_increment) {
+  uint32_t phase = phase_;
+  int32_t next_sample = next_sample_;
+  size_t size = kAudioBlockSize;
+
+  while (size--) {
+    int32_t this_sample = next_sample;
+    next_sample = 0;
+    phase += phase_increment;
+    if (phase < phase_increment) {
+      uint32_t t = phase / (phase_increment >> 16);
+      this_sample -= ThisBlepSample(t);
+      next_sample -= NextBlepSample(t);
+    }
+    next_sample += phase >> 17;
+    this_sample = (this_sample - 16384) << 1;
+    audio_buffer_.Overwrite(offset_ - (scale_ * this_sample >> 16));
+  }
+  next_sample_ = next_sample;
+  phase_ = phase;
+}
+
+void Oscillator::RenderSquare(
+    uint32_t phase_increment,
+    uint32_t pw,
+    bool integrate) {
+  uint32_t phase = phase_;
+  int32_t next_sample = next_sample_;
+  int32_t integrator_state = integrator_state_;
+  int16_t integrator_coefficient = phase_increment >> 18;
+  size_t size = kAudioBlockSize;
+
+  while (size--) {
+    int32_t this_sample = next_sample;
+    next_sample = 0;
+    phase += phase_increment;
+
+    if (!high_) {
+      if (phase >= pw) {
+        uint32_t t = (phase - pw) / (phase_increment >> 16);
+        this_sample += ThisBlepSample(t);
+        next_sample += NextBlepSample(t);
+        high_ = true;
+      }
+    }
+    if (high_ && (phase < phase_increment)) {
+      uint32_t t = phase / (phase_increment >> 16);
+      this_sample -= ThisBlepSample(t);
+      next_sample -= NextBlepSample(t);
+      high_ = false;
+    }
+    next_sample += phase < pw ? 0 : 32767;
+    this_sample = (this_sample - 16384) << 1;
+    if (integrate) {
+      integrator_state += integrator_coefficient * (this_sample - integrator_state) >> 15;
+      this_sample = integrator_state << 3;
+    }
+    audio_buffer_.Overwrite(offset_ - (scale_ * this_sample >> 16));
+  }
+  integrator_state_ = integrator_state;
+  next_sample_ = next_sample;
+  phase_ = phase;
+}
+
+void Oscillator::Render(uint8_t mode, int16_t note, bool gate) {
+  if (mode == 0 || audio_buffer_.writable() < kAudioBlockSize) {
+    return;
+  }
+
+  if ((mode & 0x80) && !gate) {
+    RenderSilence();
+    return;
+  }
+
+  uint32_t phase_increment = ComputePhaseIncrement(note);
+  switch ((mode & 0x0f) - 1) {
+    case 0:
+      RenderSaw(phase_increment);
+      break;
+    case 1:
+      RenderSquare(phase_increment, 0x40000000, false);
+      break;
+    case 2:
+      RenderSquare(phase_increment, 0x80000000, false);
+      break;
+    case 3:
+      RenderSquare(phase_increment, 0x80000000, true);
+      break;
+    case 4:
+      RenderSine(phase_increment);
+      break;
+    default:
+      RenderNoise();
+      break;
+  }
+}
 }  // namespace yarns
